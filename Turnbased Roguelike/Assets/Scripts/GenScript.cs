cf3f1858b947ba2957631972c87b626d800ada73
ewilsandman <56822177+ewilsandman@users.noreply.github.com>
2021-09-01 15:23:15 +0200

    Generation of one room recursively

--- Turnbased Roguelike/Assets/Scripts/GenScript.cs
+++ Turnbased Roguelike/Assets/Scripts/GenScript.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class GenScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+   [SerializeField] private GameObject prefab;
+   // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject newPart = Instantiate(prefab);
+        foreach (Transform child in transform)
+        {
+            if (child.CompareTag("Connector"))
+            {
+                newPart.transform.position = new Vector2(child.transform.position.x *2, child.transform.position.y);
+            }
+        }
     }
 
     // Update is called once per frame
